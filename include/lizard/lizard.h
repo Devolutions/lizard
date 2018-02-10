a9ddaf876160e579098c00716646f7bd3b4a4a67
Marc-André Moreau <mamoreau@devolutions.net>
2018-02-10 09:41:26 -0500

    lizard: initial commit

---
+++ include/lizard/lizard.h
@@ -0,0 +1,21 @@
+#ifndef LIZARD_API_H
+#define LIZARD_API_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIZARD_API_H */
